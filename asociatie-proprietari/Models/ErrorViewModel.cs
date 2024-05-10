6fb6689d9a72a5e2b53789c9a9418566ce30d09f
Niculescu Madalin <niculescumadalin60@gmail.com>
2024-05-10 20:45:51 +0300

    Add project files.

---
+++ asociatie-proprietari/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asociatie_proprietari.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
