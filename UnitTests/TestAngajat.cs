66fd569a95b223341e6e4255d04203d8506387df
Niculescu Madalin <niculescumadalin60@gmail.com>
2024-05-28 18:38:15 +0300

    Added More Unit Tests for Employees Controller

--- UnitTests/TestAngajat.cs
+++ UnitTests/TestAngajat.cs
@@ -99,5 +99,144 @@ namespace UnitTests
             Assert.That(result, Is.InstanceOf<ViewResult>(), "Expected ViewResult");
         }
 
+        [Test]
+        public async Task Create_Post_RedirectsToIndex_OnSuccess()
+        {
+            // Arrange
+            var newAngajat = new Angajat
+            {
+                AngajatId = 3,
+                Nume = "Michael",
+                Prenume = "Johnson",
+                Telefon = "5555555555",
+                Email = "michael.johnson@example.com",
+                Functie = "Analyst",
+                Salariu = 3000,
+                Bonus = 300
+            };
+
+            // Act
+            var result = await _controller.Create(newAngajat);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>(), "Expected RedirectToActionResult");
+            var redirectResult = result as RedirectToActionResult;
+            Assert.That(redirectResult.ActionName, Is.EqualTo("Index"), "Expected redirection to Index");
+            Assert.That(_context.Angajat.Count(), Is.EqualTo(3), "Expected the Angajat count to be 3");
+        }
+
+        [Test]
+        public async Task Edit_Get_ReturnsViewResult_WithAngajat()
+        {
+            // Act
+            var result = await _controller.Edit(1);
+
+            // Assert
+            var viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null, "Expected ViewResult");
+            var model = viewResult.Model as Angajat;
+            Assert.That(model, Is.Not.Null, "Expected Angajat as model");
+            Assert.That(model.AngajatId, Is.EqualTo(1), "Expected AngajatId to be 1");
+        }
+
+        [Test]
+        public async Task Edit_Get_ReturnsNotFound_WhenIdIsNull()
+        {
+            // Act
+            var result = await _controller.Edit(null);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<NotFoundResult>(), "Expected NotFoundResult");
+        }
+
+        [Test]
+        public async Task Edit_Get_ReturnsNotFound_WhenAngajatNotFound()
+        {
+            // Act
+            var result = await _controller.Edit(999);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<NotFoundResult>(), "Expected NotFoundResult");
+        }
+
+        [Test]
+        public async Task Edit_Post_RedirectsToIndex_OnSuccess()
+        {
+            // Arrange
+            var angajatToUpdate = _context.Angajat.First();
+            angajatToUpdate.Nume = "Updated Name";
+
+            // Act
+            var result = await _controller.Edit(angajatToUpdate.AngajatId, angajatToUpdate);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>(), "Expected RedirectToActionResult");
+            var redirectResult = result as RedirectToActionResult;
+            Assert.That(redirectResult.ActionName, Is.EqualTo("Index"), "Expected redirection to Index");
+
+            var updatedAngajat = _context.Angajat.Find(angajatToUpdate.AngajatId);
+            Assert.That(updatedAngajat.Nume, Is.EqualTo("Updated Name"), "Expected the Nume to be updated");
+        }
+
+        [Test]
+        public async Task Edit_Post_ReturnsNotFound_WhenIdMismatch()
+        {
+            // Arrange
+            var angajatToUpdate = _context.Angajat.First();
+
+            // Act
+            var result = await _controller.Edit(999, angajatToUpdate);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<NotFoundResult>(), "Expected NotFoundResult");
+        }
+
+        [Test]
+        public async Task Delete_Get_ReturnsViewResult_WithAngajat()
+        {
+            // Act
+            var result = await _controller.Delete(1);
+
+            // Assert
+            var viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null, "Expected ViewResult");
+            var model = viewResult.Model as Angajat;
+            Assert.That(model, Is.Not.Null, "Expected Angajat as model");
+            Assert.That(model.AngajatId, Is.EqualTo(1), "Expected AngajatId to be 1");
+        }
+
+        [Test]
+        public async Task Delete_Get_ReturnsNotFound_WhenIdIsNull()
+        {
+            // Act
+            var result = await _controller.Delete(null);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<NotFoundResult>(), "Expected NotFoundResult");
+        }
+
+        [Test]
+        public async Task Delete_Get_ReturnsNotFound_WhenAngajatNotFound()
+        {
+            // Act
+            var result = await _controller.Delete(999);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<NotFoundResult>(), "Expected NotFoundResult");
+        }
+
+        [Test]
+        public async Task Delete_Post_RedirectsToIndex_OnSuccess()
+        {
+            // Act
+            var result = await _controller.DeleteConfirmed(1);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>(), "Expected RedirectToActionResult");
+            var redirectResult = result as RedirectToActionResult;
+            Assert.That(redirectResult.ActionName, Is.EqualTo("Index"), "Expected redirection to Index");
+            Assert.That(_context.Angajat.Count(), Is.EqualTo(1), "Expected the Angajat count to be 1");
+        }
+
     }
 }
